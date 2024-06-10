b9288e3539d7997f77df8905e101e26bc55aa3d7
Jonathan-Beer <yojobeer@gmail.com>
2024-06-10 12:08:13 -0400

    changed UI elements

--- src/app.rs
+++ src/app.rs
@@ -1,19 +1,22 @@
 use egui::*;
 
-/// We derive Deserialize/Serialize so we can persist app state on shutdown.
-#[derive(serde::Deserialize, serde::Serialize)]
-#[serde(default)] // if we add new fields, give them default values when deserializing old state
-
-
+#[derive(Debug, PartialEq)]
+enum Modes {
+    Add,
+    Connect,
+    Move
+}
 
 pub struct Graphs {
 
     vertices: Vec<Pos2>,
     edges: Vec<Vec<Pos2>>,
     stroke: Stroke,
-    fill: Color32
-
+    fill: Color32,
+    mode: Modes,
+    
 }
+
 impl Default for Graphs {
     fn default() -> Self {
         Self {
@@ -21,6 +24,7 @@ impl Default for Graphs {
             edges: Default::default(),
             stroke: Stroke::new(1.0, Color32::from_rgb(25, 200, 100)),
             fill: Color32::from_rgb(50, 100, 150).linear_multiply(0.25),
+            mode: Modes::Add
         }
     }
 }
@@ -31,6 +35,24 @@ impl Graphs {
         // Use the cc.gl (a glow::Context) to create graphics shaders and buffers that you can use
         // for e.g. egui::PaintCallback.
         Self::default()
+    }
+
+    fn buttons(&mut self, ui: &mut egui::Ui) {
+        let Self {
+            vertices,
+            edges,
+            stroke,
+            fill,
+            mode,
+        } = self;
+
+        ui.add_space(16.0);
+
+        ui.horizontal(|ui| {
+            ui.selectable_value(mode, Modes::Add, "Add Vertices");
+            ui.selectable_value(mode, Modes::Connect, "Add Edges");
+            ui.selectable_value(mode, Modes::Move, "Move Vertices");
+        });
     }
 }
 
@@ -46,51 +68,18 @@ impl eframe::App for Graphs {
             egui::menu::bar(ui, |ui| {
                 // NOTE: no File->Quit on web pages!
                 let is_web = cfg!(target_arch = "wasm32");
-                if !is_web {
-                    ui.menu_button("File", |ui| {
-                        if ui.button("Quit").clicked() {
-                            ctx.send_viewport_cmd(egui::ViewportCommand::Close);
-                        }
-                    });
-                    ui.add_space(16.0);
-                }
+                
+                self.buttons(ui);
 
-                egui::widgets::global_dark_light_mode_buttons(ui);
+
             });
         });
 
         egui::CentralPanel::default().show(ctx, |ui| {
             ui.painter();
-            // The central panel the region left after adding TopPanel's and SidePanel's
-            ui.heading("eframe template");
-
-          
-
-            ui.separator();
-
-            ui.add(egui::github_link_file!(
-                "https://github.com/emilk/eframe_template/blob/main/",
-                "Source code."
-            ));
-
-            ui.with_layout(egui::Layout::bottom_up(egui::Align::LEFT), |ui| {
-                powered_by_egui_and_eframe(ui);
-                egui::warn_if_debug_build(ui);
-            });
+           
         });
     }
 }
 
-fn powered_by_egui_and_eframe(ui: &mut egui::Ui) {
-    ui.horizontal(|ui| {
-        ui.spacing_mut().item_spacing.x = 0.0;
-        ui.label("Powered by ");
-        ui.hyperlink_to("egui", "https://github.com/emilk/egui");
-        ui.label(" and ");
-        ui.hyperlink_to(
-            "eframe",
-            "https://github.com/emilk/egui/tree/master/crates/eframe",
-        );
-        ui.label(".");
-    });
-}
+
