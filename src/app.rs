35a6d575de629567987cc52cc58264ac446b0de5
Jonathan-Beer <yojobeer@gmail.com>
2024-06-11 15:11:51 -0400

    vertices are now actually circles

--- src/app.rs
+++ src/app.rs
@@ -1,4 +1,5 @@
 use egui::*;
+use epaint::CircleShape;
 
 #[derive(Debug, PartialEq, Copy, Clone)]
 enum Modes {
@@ -8,7 +9,7 @@ enum Modes {
 }
 
 fn are_incident(e1: Vec<[Pos2; 2]>, e2: Vec<[Pos2; 2]>) -> bool{
-    if(e1[0] == e2[0] || e1[1] == e2[0] || e1[0] == e2[1] || e1[1] == e2[1]){
+    if e1[0] == e2[0] || e1[1] == e2[0] || e1[0] == e2[1] || e1[1] == e2[1]{
         return true;
     }
     return false;
@@ -21,32 +22,30 @@ fn intersect(e1: Vec<[Pos2; 2]>, e2: Vec<[Pos2; 2]>) -> bool{
 
 pub struct Graphs {
 
-    vertices: Vec<Pos2>,
-    edges: Vec<[Pos2; 2]>,
+    vertices: Vec<CircleShape>,
+    edges: Vec<[CircleShape; 2]>,
     stroke: Stroke,
     fill: Color32,
     mode: Modes,
+    radius: f32
     
 }
 
 impl Default for Graphs {
     fn default() -> Self {
         Self {
-            vertices: vec![Pos2::new(100.0,100.0)],
+            vertices: Default::default(),
             edges: Default::default(),
             stroke: Stroke::new(1.0, Color32::from_rgb(25, 200, 100)),
             fill: Color32::from_rgb(50, 100, 150).linear_multiply(0.25),
-            mode: Modes::Add
+            mode: Modes::Add,
+            radius: 12.0
         }
         
     }
 }
 impl Graphs {
     pub fn new(_cc: &eframe::CreationContext<'_>) -> Self {
-        // Customize egui here with cc.egui_ctx.set_fonts and cc.egui_ctx.set_visuals.
-        // Restore app state using cc.storage (requires the "persistence" feature).
-        // Use the cc.gl (a glow::Context) to create graphics shaders and buffers that you can use
-        // for e.g. egui::PaintCallback.
         Self::default()
     }
 
@@ -57,9 +56,10 @@ impl Graphs {
             stroke,
             fill,
             mode,
+            radius,
         } = self;
 
-        ui.add_space(24.0);
+        ui.add_space(12.0);
 
         ui.horizontal(|ui| {
             ui.selectable_value(mode, Modes::Add, "Add Vertices");
@@ -82,10 +82,24 @@ impl Graphs {
                 });
 
                 
-        });
-        
+        });        
     }
 
+    fn onclick(&mut self, ui: &mut egui::Ui) {
+        let (mut response, painter) = ui.allocate_painter(ui.available_size_before_wrap(), Sense::drag());
+        let to_screen = emath::RectTransform::from_to(
+            Rect::from_min_size(Pos2::ZERO, response.rect.square_proportions()),
+            response.rect,
+        );
+        let from_screen = to_screen.inverse();
+        if let Some(pointer_pos) = response.interact_pointer_pos() {
+            if self.mode == Modes::Add {
+                let vert = CircleShape::filled(pointer_pos, self.radius, self.fill);
+                self.vertices.push(vert);
+            }
+        }
+
+    }
 }
 
 
@@ -94,6 +108,7 @@ impl eframe::App for Graphs {
    
     /// Called each time the UI needs repainting, which may be many times per second.
     fn update(&mut self, ctx: &egui::Context, _frame: &mut eframe::Frame) {
+
         // Put your widgets into a `SidePanel`, `TopBottomPanel`, `CentralPanel`, `Window` or `Area`.
         // For inspiration and more examples, go to https://emilk.github.io/egui
         egui::TopBottomPanel::top("top_panel").show(ctx, |ui| {
@@ -101,22 +116,24 @@ impl eframe::App for Graphs {
 
             egui::menu::bar(ui, |ui| {
                 self.buttons(ui);
-
+                
             });
         });
 
         egui::CentralPanel::default().show(ctx, |ui| {
+            self.onclick(ui);
             ui.painter();
             let painter = ui.painter();
 
             let edgelist = self.edges.clone();
             for edge in edgelist {
-                painter.line_segment(edge, self.stroke);
+                let cordedge = [edge[0].center, edge[1].center];
+                painter.line_segment(cordedge, self.stroke);
             }
 
             let verlist = self.vertices.clone();
             for vertex in verlist {
-                painter.circle(vertex, 10.0, self.fill, Stroke::new(0.0, Color32::from_rgb(0, 0,0)));
+                painter.circle(vertex.center, 10.0, self.fill, Stroke::new(0.0, Color32::from_rgb(0, 0,0)));
             }
         });
     }
